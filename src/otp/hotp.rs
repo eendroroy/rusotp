73477a72bd3031599ee85f930f759fcedf3c6f7f
indrajit <eendroroy@gmail.com>
2025-01-13 14:24:52 +0600

    refactor codebase

--- src/otp/hotp.rs
+++ src/otp/hotp.rs
@@ -9,32 +9,34 @@ pub struct HOTP {
 impl HOTP {
     pub fn new(secret: &str, length: u8, radix: u8) -> Result<HOTP, &'static str> {
         if secret.len() < 1 {
-            return Err("Secret must not be empty");
+            Err("Secret must not be empty")
+        } else if length < 4 {
+            Err("Length must be greater than or equal to 4")
+        } else if radix < 2 || radix > 36 {
+            Err("Radix must be between 2 and 36 inclusive")
+        } else {
+            Ok(Self {
+                secret: Vec::from(secret),
+                length,
+                radix,
+            })
         }
-
-        if length < 4 {
-            return Err("Length must be greater than or equal to 4");
-        }
-
-        if radix < 2 || radix > 36 {
-            return Err("Radix must be between 2 and 36 inclusive");
-        }
-
-        Ok(Self {
-            secret: Vec::from(secret),
-            length,
-            radix,
-        })
     }
 
     pub fn generate(&self, counter: u64) -> Result<String, &'static str> {
         if counter < 1 {
-            return Err("Counter must be greater than or equal to 1");
+            Err("Counter must be greater than or equal to 1")
+        } else {
+            Ok(otp(counter, self.secret.clone(), self.length, self.radix))
         }
-        Ok(otp(counter, self.secret.clone(), self.length, self.radix))
     }
 
-    pub fn verify(&self, otp: &str, counter: u64, retries: u64) -> Result<Option<u64>, &'static str> {
+    pub fn verify(
+        &self,
+        otp: &str,
+        counter: u64,
+        retries: u64,
+    ) -> Result<Option<u64>, &'static str> {
         if self.length != otp.len() as u8 {
             return Err("OTP length does not match the length of the HOTP configuration");
         }
@@ -53,19 +55,21 @@ impl HOTP {
 
     pub fn provisioning_uri(&self, name: &str, initial_count: u64) -> Result<String, &'static str> {
         if self.length != 6 {
-            return Err("HOTP length must be 6");
+            Err("HOTP length must be 6")
+        } else if self.radix != 10 {
+            Err("HOTP radix must be 10")
+        } else {
+            let query = format!(
+                "secret={}&counter={}",
+                urlencoding::encode(&String::from_utf8_lossy(&self.secret)),
+                initial_count
+            );
+
+            Ok(format!(
+                "otpauth://hotp/{}?{}",
+                urlencoding::encode(name),
+                query
+            ))
         }
-
-        if self.radix != 10 {
-            return Err("HOTP radix must be 10");
-        }
-
-        let query = format!(
-            "secret={}&counter={}",
-            urlencoding::encode(&String::from_utf8_lossy(&self.secret)),
-            initial_count
-        );
-
-        Ok(format!("otpauth://hotp/{}?{}", urlencoding::encode(name), query))
     }
 }
