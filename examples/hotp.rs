92172321dbb047bf3520d1490e58a99fd3ea8e2b
indrajit <eendroroy@gmail.com>
2025-01-13 15:14:13 +0600

    add examples

---
+++ examples/hotp.rs
@@ -0,0 +1,29 @@
+use rusotp::HOTP;
+
+fn main() {
+    let secret = "12345678901234567890";
+    let hotp_tool = match HOTP::new(secret, 6, 10) {
+        Ok(hotp_tool) => hotp_tool,
+        Err(e) => panic!("{}", e),
+    };
+
+    let hotp = match HOTP::generate(&hotp_tool, 1) {
+        Ok(hotp) => hotp,
+        Err(e) => panic!("{}", e),
+    };
+
+    let hotp_is_valid = match HOTP::verify(&hotp_tool, &*hotp, 1, 10) {
+        Ok(verified) => verified.is_some(),
+        Err(e) => panic!("{}", e),
+    };
+
+    let hotp_provisioning_uri = match hotp_tool.provisioning_uri("test", 0) {
+        Ok(hotp_provisioning_uri) => hotp_provisioning_uri,
+        Err(e) => panic!("{}", e),
+    };
+
+    println!(
+        "HOTP: {}, Valid: {}, Url: {}",
+        hotp, hotp_is_valid, hotp_provisioning_uri
+    );
+}
---
+++ examples/totp.rs
@@ -0,0 +1,30 @@
+use rusotp::TOTP;
+
+fn main() {
+    let secret = "12345678901234567890";
+
+    let totp_tool = match TOTP::new(secret, "IAM", 6, 10, 30) {
+        Ok(totp_tool) => totp_tool,
+        Err(e) => panic!("{}", e),
+    };
+
+    let totp = match TOTP::at_timestamp(&totp_tool, 31) {
+        Ok(totp) => totp,
+        Err(e) => panic!("{}", e),
+    };
+
+    let totp_is_valid = match TOTP::verify(&totp_tool, &*totp, 59, None, 0, 0) {
+        Ok(verified) => verified.is_some(),
+        Err(e) => panic!("{}", e),
+    };
+
+    let totp_provisioning_uri = match TOTP::provisioning_uri(&totp_tool, "test@mail.com") {
+        Ok(totp_provisioning_uri) => totp_provisioning_uri,
+        Err(e) => panic!("{}", e),
+    };
+
+    println!(
+        "TOTP: {}, Valid: {}, Url: {}",
+        totp, totp_is_valid, totp_provisioning_uri
+    );
+}
