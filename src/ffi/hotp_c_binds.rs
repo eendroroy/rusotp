6b83a6571c9a72fa119c9e0d729fcf7d5a475c2c
indrajit <eendroroy@gmail.com>
2025-01-19 18:39:36 +0600

    add c binding documentations

--- src/ffi/hotp_c_binds.rs
+++ src/ffi/hotp_c_binds.rs
@@ -2,6 +2,13 @@ use crate::ffi::converter::{to_hotp, to_str};
 use std::ffi::CStr;
 use std::os::raw::{c_char, c_ulong, c_ushort};
 
+/// Configuration for HOTP (HMAC-based One-Time Password).
+///
+/// # Fields
+/// - `algorithm`: A pointer to a C string representing the hashing algorithm (e.g., "SHA1").
+/// - `secret`: A pointer to a C string representing the shared secret key.
+/// - `length`: The length of the generated OTP.
+/// - `radix`: The base (radix) for the OTP (e.g., 10 for decimal).
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct HotpConfig {
@@ -11,6 +18,24 @@ pub struct HotpConfig {
     pub radix: c_ushort,
 }
 
+/// Generates an HOTP (HMAC-based One-Time Password) based on the provided configuration and counter.
+///
+/// # Arguments
+///
+/// * `config` - A `HotpConfig` struct containing the configuration for the HOTP generation.
+/// * `counter` - A counter value used in the HOTP generation.
+///
+/// # Returns
+///
+/// A pointer to a C string containing the generated HOTP. The caller is responsible for freeing the memory.
+///
+/// # Panics
+///
+/// This function will panic if the HOTP generation fails.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers and returns a raw pointer.
 #[export_name = "generate_hotp"]
 pub unsafe extern "C" fn c_generate_hotp(config: HotpConfig, counter: c_ulong) -> *mut c_char {
     let hotp = to_hotp(config);
@@ -21,6 +46,25 @@ pub unsafe extern "C" fn c_generate_hotp(config: HotpConfig, counter: c_ulong) -
     }
 }
 
+/// Generates a provisioning URI for HOTP (HMAC-based One-Time Password) based on the provided configuration, name, and counter.
+///
+/// # Arguments
+///
+/// * `config` - A `HotpConfig` struct containing the configuration for the HOTP generation.
+/// * `name` - A pointer to a C string representing the name of the user or account.
+/// * `counter` - A counter value used in the HOTP generation.
+///
+/// # Returns
+///
+/// A pointer to a C string containing the provisioning URI. The caller is responsible for freeing the memory.
+///
+/// # Panics
+///
+/// This function will panic if the name is null or if the URI generation fails.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers and returns a raw pointer.
 #[export_name = "hotp_provisioning_uri"]
 pub unsafe extern "C" fn c_hotp_provisioning_uri(
     config: HotpConfig,
@@ -39,6 +83,26 @@ pub unsafe extern "C" fn c_hotp_provisioning_uri(
     }
 }
 
+/// Verifies an HOTP (HMAC-based One-Time Password) based on the provided configuration, OTP, counter, and retries.
+///
+/// # Arguments
+///
+/// * `config` - A `HotpConfig` struct containing the configuration for the HOTP verification.
+/// * `otp` - A pointer to a C string representing the OTP to be verified.
+/// * `counter` - A counter value used in the HOTP verification.
+/// * `retries` - The number of retries allowed for the HOTP verification.
+///
+/// # Returns
+///
+/// A boolean value indicating whether the OTP is verified (`true`) or not (`false`).
+///
+/// # Panics
+///
+/// This function will panic if the OTP is null or if the HOTP verification fails.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers.
 #[export_name = "verify_hotp"]
 pub unsafe extern "C" fn c_verify_hotp(
     config: HotpConfig,
--- src/ffi/totp_c_binds.rs
+++ src/ffi/totp_c_binds.rs
@@ -1,6 +1,14 @@
 use crate::ffi::converter::{to_str, to_totp};
 use std::os::raw::{c_char, c_ulong, c_ushort};
 
+/// Configuration for TOTP (Time-based One-Time Password).
+///
+/// # Fields
+/// - `algorithm`: A pointer to a C string representing the hashing algorithm (e.g., "SHA1").
+/// - `secret`: A pointer to a C string representing the shared secret key.
+/// - `length`: The length of the generated OTP.
+/// - `radix`: The base (radix) for the OTP (e.g., 10 for decimal).
+/// - `interval`: The time interval in seconds for the TOTP generation.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct TotpConfig {
@@ -11,6 +19,23 @@ pub struct TotpConfig {
     pub interval: c_ushort,
 }
 
+/// Generates a TOTP (Time-based One-Time Password) based on the provided configuration for the current time.
+///
+/// # Arguments
+///
+/// * `config` - A `TotpConfig` struct containing the configuration for the TOTP generation.
+///
+/// # Returns
+///
+/// A pointer to a C string containing the generated TOTP. The caller is responsible for freeing the memory.
+///
+/// # Panics
+///
+/// This function will panic if the TOTP generation fails.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers and returns a raw pointer.
 #[export_name = "generate_totp_now"]
 pub unsafe extern "C" fn c_generate_totp_now(config: TotpConfig) -> *mut c_char {
     let totp = to_totp(config);
@@ -20,6 +45,24 @@ pub unsafe extern "C" fn c_generate_totp_now(config: TotpConfig) -> *mut c_char 
     }
 }
 
+/// Generates a TOTP (Time-based One-Time Password) based on the provided configuration and timestamp.
+///
+/// # Arguments
+///
+/// * `config` - A `TotpConfig` struct containing the configuration for the TOTP generation.
+/// * `timestamp` - A timestamp value used in the TOTP generation.
+///
+/// # Returns
+///
+/// A pointer to a C string containing the generated TOTP. The caller is responsible for freeing the memory.
+///
+/// # Panics
+///
+/// This function will panic if the TOTP generation fails.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers and returns a raw pointer.
 #[export_name = "generate_totp_at"]
 pub unsafe extern "C" fn c_generate_totp_at(config: TotpConfig, timestamp: c_ulong) -> *mut c_char {
     let totp = to_totp(config);
@@ -29,6 +72,28 @@ pub unsafe extern "C" fn c_generate_totp_at(config: TotpConfig, timestamp: c_ulo
     }
 }
 
+/// Verifies a TOTP (Time-based One-Time Password) based on the provided configuration, OTP, timestamp, and drift parameters.
+///
+/// # Arguments
+///
+/// * `config` - A `TotpConfig` struct containing the configuration for the TOTP verification.
+/// * `otp` - A pointer to a C string representing the OTP to be verified.
+/// * `timestamp` - A timestamp value used in the TOTP verification.
+/// * `after` - The number of time steps after the current time to allow for verification.
+/// * `drift_ahead` - The number of time steps ahead of the current time to allow for verification.
+/// * `drift_behind` - The number of time steps behind the current time to allow for verification.
+///
+/// # Returns
+///
+/// A boolean value indicating whether the OTP is verified (`true`) or not (`false`).
+///
+/// # Panics
+///
+/// This function will panic if the OTP is null or if the TOTP verification fails.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers.
 #[export_name = "verify_totp"]
 pub unsafe extern "C" fn c_verify_totp(
     config: TotpConfig,
@@ -56,6 +121,25 @@ pub unsafe extern "C" fn c_verify_totp(
     }
 }
 
+/// Generates a provisioning URI for TOTP (Time-based One-Time Password) based on the provided configuration, issuer, and name.
+///
+/// # Arguments
+///
+/// * `config` - A `TotpConfig` struct containing the configuration for the TOTP generation.
+/// * `issuer` - A pointer to a C string representing the issuer of the TOTP.
+/// * `name` - A pointer to a C string representing the name of the user or account.
+///
+/// # Returns
+///
+/// A pointer to a C string containing the provisioning URI. The caller is responsible for freeing the memory.
+///
+/// # Panics
+///
+/// This function will panic if the issuer or name is null or if the URI generation fails.
+///
+/// # Safety
+///
+/// This function is unsafe because it dereferences raw pointers and returns a raw pointer.
 #[export_name = "totp_provisioning_uri"]
 pub unsafe extern "C" fn c_totp_provisioning_uri(
     config: TotpConfig,
