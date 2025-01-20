9766b503d49ac9a6228dfac35083d687827a7578
indrajit <eendroroy@gmail.com>
2025-01-20 14:16:29 +0600

    update c examples

--- c_examples/hotp_fn.cpp
+++ c_examples/hotp_fn.cpp
@@ -9,32 +9,31 @@ int main() {
         unsigned long counter;
         const char* otp;
     } data[] = {
-        {{"SHA256", secret, 6, 10}, 1, "247374"},
-        {{"SHA256", secret, 6, 10}, 2, "254785"},
-        {{"SHA256", secret, 6, 10}, 3, "496144"},
-        {{"SHA256", secret, 6, 16}, 1, "687B4E"},
-        {{"SHA256", secret, 6, 24}, 1, "N7C1B6"},
-        {{"SHA256", secret, 6, 36}, 1, "M16ONI"},
-        {{"SHA256", secret, 8, 10}, 100, "93583477"},
-        {{"SHA256", secret, 8, 16}, 100, "23615D75"},
-        {{"SHA256", secret, 8, 24}, 100, "032D2EKL"},
-        {{"SHA256", secret, 8, 36}, 100, "009TEJXX"},
-        {{"SHA256", secret, 4, 36}, 1, "6ONI"},
-        {{"SHA256", secret, 4, 36}, 2, "KYWX"},
-        {{"SHA256", secret, 4, 36}, 3, "ERBK"},
-        {{"SHA256", secret, 4, 36}, 4, "ROTO"},
+        {{"SHA1", secret, 6, 10}, 1},
+        {{"SHA256", secret, 6, 10}, 2},
+        {{"SHA256", secret, 6, 10}, 3},
+        {{"SHA1", secret, 6, 16}, 1},
+        {{"SHA256", secret, 6, 24}, 1},
+        {{"SHA256", secret, 6, 36}, 1},
+        {{"SHA256", secret, 8, 10}, 100},
+        {{"SHA256", secret, 8, 16}, 100},
+        {{"SHA1", secret, 8, 24}, 100},
+        {{"SHA256", secret, 8, 36}, 100},
+        {{"SHA256", secret, 4, 36}, 1},
+        {{"SHA256", secret, 4, 36}, 2},
+        {{"SHA256", secret, 4, 36}, 3},
+        {{"SHA256", secret, 4, 36}, 4},
     };
 
     for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
         HotpConfig config = data[i].config;
         unsigned long counter = data[i].counter;
-        const char* otp_value = data[i].otp;
 
-        char *otp = generate_hotp(config, counter);
+        const char *otp = generate_hotp(config, counter);
         const char *verified = verify_hotp(config, otp, counter, 0) ? "true" : "false";
 
         if (config.radix == 10 && config.length == 6 && strcmp(config.algorithm, "SHA1") == 0) {
-            char *uri = hotp_provisioning_uri(config, "rusotp", counter);
+            const char *uri = hotp_provisioning_uri(config, "rusotp", counter);
             printf("LENGTH: %d, RADIX: %d, COUNTER: %lu \tHOTP : %s \tVERIFIED : %s \tURI : %s\n", config.length, config.radix, counter, otp, verified, uri);
         } else {
             printf("LENGTH: %d, RADIX: %d, COUNTER: %lu \tHOTP : %s \tVERIFIED : %s\n", config.length, config.radix, counter, otp, verified);
--- c_examples/totp_fn.cpp
+++ c_examples/totp_fn.cpp
@@ -9,26 +9,29 @@ int main() {
         unsigned long timestamp;
         const char* otp;
     } data[] = {
-        {{"SHA256", secret, 6, 10, 10}, 10000, "959738"},
-        {{"SHA256", secret, 6, 10, 20}, 10000, "946818"},
-        {{"SHA256", secret, 6, 10, 30}, 10000, "474706"},
-        {{"SHA256", secret, 6, 16, 1}, 10000, "A4AC65"},
-        {{"SHA256", secret, 6, 24, 2}, 10000, "HIH7EE"},
-        {{"SHA256", secret, 6, 10, 30}, 300, "586609"},
-        {{"SHA256", secret, 8, 10, 100}, 10000, "93583477"},
-        {{"SHA256", secret, 8, 16, 100}, 10000, "23615D75"},
-        {{"SHA256", secret, 8, 24, 100}, 10000, "032D2EKL"},
-        {{"SHA256", secret, 8, 36, 100}, 10000, "009TEJXX"},
-        {{"SHA256", secret, 4, 36, 1}, 10000, "D55X"},
-        {{"SHA256", secret, 4, 36, 200}, 10000, "GZ11"},
-        {{"SHA256", secret, 4, 36, 31}, 10000, "XJTQ"},
-        {{"SHA256", secret, 4, 36, 44}, 10000, "8KE5"},
+        {{"SHA256", secret, 6, 10, 10}, 10000},
+        {{"SHA256", secret, 6, 10, 20}, 10000},
+        {{"SHA1", secret, 6, 10, 30}, 10000},
+        {{"SHA256", secret, 6, 16, 1}, 10000},
+        {{"SHA256", secret, 6, 24, 2}, 10000},
+        {{"SHA1", secret, 6, 10, 30}, 300},
+        {{"SHA256", secret, 8, 10, 100}, 10000},
+        {{"SHA256", secret, 8, 16, 100}, 10000},
+        {{"SHA256", secret, 8, 24, 100}, 10000},
+        {{"SHA256", secret, 8, 36, 100}, 10000},
+        {{"SHA256", secret, 4, 36, 1}, 10000},
+        {{"SHA256", secret, 4, 36, 200}, 10000},
+        {{"SHA256", secret, 4, 36, 31}, 10000},
+        {{"SHA256", secret, 4, 36, 44}, 10000},
     };
 
     for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
         TotpConfig config = data[i].config;
         unsigned long timestamp = data[i].timestamp;
-        const char* otp = data[i].otp;
+
+        const char *otp_now =  generate_totp_now(config);
+        const char *otp_at = generate_totp_at(config, timestamp);
+        const char *verified = verify_totp(config, otp_at, timestamp, 0, 0, 0) ? "true" : "false";
 
         if (config.length == 6 && config.radix == 10 && config.interval == 30 && strcmp(config.algorithm, "SHA1") == 0) {
             printf(
@@ -37,9 +40,9 @@ int main() {
                 config.radix,
                 config.interval,
                 timestamp,
-                generate_totp_now(config),
-                generate_totp_at(config, timestamp),
-                verify_totp(config, otp, timestamp, 0, 0, 0) ? "true" : "false",
+                otp_now,
+                otp_at,
+                verified,
                 totp_provisioning_uri(config, "rusotp", "user@email.mail"));
         } else {
             printf(
@@ -48,9 +51,10 @@ int main() {
                 config.radix,
                 config.interval,
                 timestamp,
-                generate_totp_now(config),
-                generate_totp_at(config, timestamp),
-                verify_totp(config, otp, timestamp, 0, 0, 0) ? "true" : "false");
+                otp_now,
+                otp_at,
+                verified
+            );
         }
     }
 
