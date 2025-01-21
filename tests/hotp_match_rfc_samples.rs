77354623e74c4f6034d32993005f1c5c9ca228b0
indrajit <eendroroy@gmail.com>
2025-01-21 19:31:07 +0600

    add tests to match with RFC provided samples

---
+++ tests/hotp_match_rfc_samples.rs
@@ -0,0 +1,26 @@
+use rusotp::{Algorithm, HOTP};
+
+const SECRET: &str = "12345678901234567890";
+
+#[test]
+fn otp_should_be_generated() {
+    vec![
+        (0, "755224"),
+        (1, "287082"),
+        (2, "359152"),
+        (3, "969429"),
+        (4, "338314"),
+        (5, "254676"),
+        (6, "287922"),
+        (7, "162583"),
+        (8, "399871"),
+        (9, "520489"),
+    ]
+    .iter()
+    .for_each(|(counter, otp)| {
+        let hotp = HOTP::new(Algorithm::SHA1, SECRET, 6, 10).unwrap();
+        let result = hotp.generate(*counter);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), *otp);
+    });
+}
---
+++ tests/totp_match_rfc_samples.rs
@@ -0,0 +1,36 @@
+use rusotp::{Algorithm, TOTP};
+
+const SECRET_SHA1: &str = "12345678901234567890";
+const SECRET_SHA256: &str = "12345678901234567890123456789012";
+const SECRET_SHA512: &str = "1234567890123456789012345678901234567890123456789012345678901234";
+
+#[test]
+fn otp_should_be_generated() {
+    vec![
+        (SECRET_SHA1, 59, "94287082", Algorithm::SHA1),
+        (SECRET_SHA256, 59, "46119246", Algorithm::SHA256),
+        (SECRET_SHA512, 59, "90693936", Algorithm::SHA512),
+        (SECRET_SHA1, 1111111109, "07081804", Algorithm::SHA1),
+        (SECRET_SHA256, 1111111109, "68084774", Algorithm::SHA256),
+        (SECRET_SHA512, 1111111109, "25091201", Algorithm::SHA512),
+        (SECRET_SHA1, 1111111111, "14050471", Algorithm::SHA1),
+        (SECRET_SHA256, 1111111111, "67062674", Algorithm::SHA256),
+        (SECRET_SHA512, 1111111111, "99943326", Algorithm::SHA512),
+        (SECRET_SHA1, 1234567890, "89005924", Algorithm::SHA1),
+        (SECRET_SHA256, 1234567890, "91819424", Algorithm::SHA256),
+        (SECRET_SHA512, 1234567890, "93441116", Algorithm::SHA512),
+        (SECRET_SHA1, 2000000000, "69279037", Algorithm::SHA1),
+        (SECRET_SHA256, 2000000000, "90698825", Algorithm::SHA256),
+        (SECRET_SHA512, 2000000000, "38618901", Algorithm::SHA512),
+        (SECRET_SHA1, 20000000000, "65353130", Algorithm::SHA1),
+        (SECRET_SHA256, 20000000000, "77737706", Algorithm::SHA256),
+        (SECRET_SHA512, 20000000000, "47863826", Algorithm::SHA512),
+    ]
+    .iter()
+    .for_each(|(secret, timestamp, otp, algorithm)| {
+        let totp = TOTP::new(*algorithm, secret, 8, 10, 30).unwrap();
+        let result = totp.generate_at(*timestamp);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), *otp);
+    });
+}
