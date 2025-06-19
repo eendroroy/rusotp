2ce1fc7caba0b2c880c36a94db8c6aaaebb06437
indrajit <eendroroy@gmail.com>
2025-06-19 12:32:57 +0600

    add build support for linux, macos, windows

--- build.rs
+++ build.rs
@@ -6,18 +6,21 @@ fn main() {
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     let package = env!("CARGO_PKG_NAME");
 
+    // Generate C++ header
     cbindgen::Builder::new()
-        .with_crate(&manifest_dir)
+        .with_crate(manifest_dir)
         .with_language(cbindgen::Language::Cxx)
         .generate()
-        .unwrap()
+        .unwrap_or_else(|e| panic!("Failed to generate bindings: {}", e))
         .write_to_file("contrib/rusotp.hpp");
 
+    // Construct shared object path
     let mut shared_object_dir = PathBuf::from(manifest_dir);
     shared_object_dir.push("target");
     shared_object_dir.push(env::var("PROFILE").unwrap());
-    let shared_object_dir = shared_object_dir.as_path().to_string_lossy();
+    let shared_object_dir = shared_object_dir.as_path().to_string_lossy().replace('\\', "/"); // Prevent backslash issues on Windows
 
+    // Emit INLINE_C_RS_CFLAGS based on platform
     #[cfg(target_os = "linux")]
     println!(
         "cargo:rustc-env=INLINE_C_RS_CFLAGS=-I{I} -L{L} -D_DEBUG -D_GNU_SOURCE",
@@ -39,7 +42,7 @@ fn main() {
         L = shared_object_dir,
     );
 
-    let lib_name = format!("{}{}{}", DLL_PREFIX, package, DLL_SUFFIX,);
-
+    // Linker flags
+    let lib_name = format!("{}{}{}", DLL_PREFIX, package, DLL_SUFFIX);
     println!("cargo:rustc-env=INLINE_C_RS_LDFLAGS={}/{}", shared_object_dir, lib_name);
 }
