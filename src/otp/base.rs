d97d600fe27e7df32b280e199a3b12aad938ad5a
indrajit <eendroroy@gmail.com>
2025-06-23 17:54:38 +0600

    add support for longer length

--- src/otp/base.rs
+++ src/otp/base.rs
@@ -5,12 +5,13 @@ use std::ops::Rem;
 
 pub(crate) fn otp(algorithm: &Algorithm, secret: Vec<u8>, length: u8, radix: u8, counter: u64) -> OtpResult<String> {
     match otp_bin_code(algorithm, secret, counter) {
-        Ok(otp_bin_code) => Ok(format!(
-            "{:0>width$}",
-            BigUint::to_str_radix(&BigUint::from(otp_bin_code.rem((radix as u64).pow(length as u32))), radix as u32,),
-            width = length as usize
-        )
-        .to_uppercase()),
+        Ok(otp_bin_code) => {
+            let value = BigUint::to_str_radix(
+                &BigUint::from(otp_bin_code).rem(BigUint::from(radix as u64).pow(length as u32)),
+                radix as u32,
+            );
+            Ok(format!("{:0>width$}", value, width = length as usize).to_uppercase())
+        }
         Err(e) => Err(Box::new(OtpGenericError(e.to_string()))),
     }
 }
